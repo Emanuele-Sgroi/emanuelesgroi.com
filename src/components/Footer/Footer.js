6ed35d9b610b3a68b7c638af1a8d85d50bbe6593
Emanuele Sgroi <nunzioemanuelesgroi@gmail.com>
2024-12-04 00:04:06 +0000

    Made Footer responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,27 +6,27 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="hidden w-full mt-auto pt-12 pb-10 px-4">
-      <div className="w-full center gap-2">
+    <footer className=" w-full mt-auto pt-10 md:pt-12 pb-10 px-4">
+      <div className="w-full center gap-2 max-md:flex-col">
         <Link
           href="/"
-          className="text-text-primary center text-lg w-[26px] h-[26px] border border-text-primary center"
+          className="text-text-primary center text-[22px] md:text-lg w-[38px] md:w-[26px] h-[38px] md:h-[26px] border border-text-primary center max-md:mb-2"
         >
           E
         </Link>
-        <p className="text-xs  text-text-secondary ">
+        <p className="text-xs text-center text-text-secondary ">
           &copy; {currentYear} Made with ❤️ by{" "}
           <span className="font-semibold">Emanuele Sgroi</span>
         </p>
-        <div className="h-px w-2 bg-text-secondary mt-1" />
-        <p className=" text-xs text-text-secondary center gap-[3px]">
+        <div className="max-md:hidden h-px w-2 bg-text-secondary mt-1" />
+        <p className="text-center text-xs text-text-secondary center gap-[3px]">
           Inspired by the design of GitHub <IoLogoGithub size={22} />
         </p>
-        <div className="h-px w-2 bg-light-text-secondary dark:bg-dark-text-secondary mt-1" />
+        <div className="max-md:hidden h-px w-2 bg-text-secondary mt-1" />
         <Link
           href="https://github.com/Emanuele-Sgroi/emanuelesgroi.com"
           target="_blank"
-          className="text-xs text-text-secondary hover:text-accent-extra "
+          className="text-center text-xs text-text-secondary hover:text-accent-extra "
         >
           Give it a star ⭐
         </Link>
