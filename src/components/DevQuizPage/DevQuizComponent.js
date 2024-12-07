785bf0b1a16ff497e6ab8059e3f468d7befdbc57
Emanuele Sgroi <nunzioemanuelesgroi@gmail.com>
2024-12-07 20:04:46 +0000

    Created Dev Quiz Page and Question JS file

---
+++ src/components/DevQuizPage/DevQuizComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DevQuizComponent = () => {
+  return (
+    <div className="main-container">
+      <div className="borded-container md:!min-h-[800px]"></div>
+    </div>
+  );
+};
+
+export default DevQuizComponent;
