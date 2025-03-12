5a6a28611819d7bf4cbfbaf48f05dd99f423a013
Emanuele Sgroi <nunzioemanuelesgroi@gmail.com>
2025-03-12 08:18:17 +0000

    Changed splashscreen

--- src/context/SplashScreenProvider.js
+++ src/context/SplashScreenProvider.js
@@ -1,16 +1,11 @@
 "use client";
 
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 const SplashScreenContext = createContext();
 
 export const SplashScreenProvider = ({ children }) => {
-  const [showSplash, setShowSplash] = useState(false);
-
-  useEffect(() => {
-    // Set showSplash to true only after the component mounts on the client
-    setShowSplash(true);
-  }, []);
+  const [showSplash, setShowSplash] = useState(true);
 
   const hideSplashScreen = () => {
     setShowSplash(false);
@@ -24,25 +19,3 @@ export const SplashScreenProvider = ({ children }) => {
 };
 
 export default SplashScreenContext;
-
-// "use client";
-
-// import { createContext, useState } from "react";
-
-// const SplashScreenContext = createContext();
-
-// export const SplashScreenProvider = ({ children }) => {
-//   const [showSplash, setShowSplash] = useState(true);
-
-//   const hideSplashScreen = () => {
-//     setShowSplash(false);
-//   };
-
-//   return (
-//     <SplashScreenContext.Provider value={{ showSplash, hideSplashScreen }}>
-//       {children}
-//     </SplashScreenContext.Provider>
-//   );
-// };
-
-// export default SplashScreenContext;
